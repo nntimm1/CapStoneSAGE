8fbfdb334f9d6027a40da5c0925f4e35240c051c
Nicolas Timm <nntimm1@gmail.com>
2020-03-13 16:16:27 -0500

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SAGEWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
