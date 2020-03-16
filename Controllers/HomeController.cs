9d547ec75755dc0f1cd55beb32d51ba01c945716
Nicolas Timm <nntimm1@gmail.com>
2020-03-16 11:10:39 -0500

    Added About and contact pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,15 @@ namespace SAGEWebsite.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
